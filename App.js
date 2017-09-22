ad819af62fde029e7507b0e3a65e67125fc1e2f0
Matthew Davies <matthew.davies@freetimers.com>
2017-09-22 14:35:37 +0100

    Refactoring and prep

--- App.js
+++ App.js
@@ -1,47 +1,43 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
+import Background from 'images/galaxy-wallpaper-30.jpg';
 
-export default class Movies extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true
-        }
-    }
-
-    componentDidMount() {
-        return fetch('https://raw.githubusercontent.com/twotoan/react-native/master/dataTest/test_query.json')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-                this.setState({
-                    isLoading: false,
-                    dataSource: ds.cloneWithRows(responseJson.counties),
-                }, function() {
-                    // do something with new state
-                });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
+export default class App extends Component {
     render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={{flex: 1, paddingTop: 20}}>
-                    <ActivityIndicator />
-                </View>
-            );
-        }
-
         return (
-            <View style={{flex: 1, paddingTop: 20}}>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData.cu_id}, {rowData.cu_name}</Text>}
-                />
-            </View>
-        );
+            <Login></Login>
+        )
     }
 }
+
+export class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Button title="Submit" accessibilityLabel="Submit Details" style={styles.buttonStyle}>Submit</Button>
+            </View>
+        )
+    }
+},
+
+export class Questions extends Component {
+
+}
+
+export class Scores extends Component {
+
+}
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: 'blue',
+    },
+    content: {
+        backgroundImage: `url(${Background})`,
+    },
+    buttonStyle: {
+        backgroundColor: 'purple',
+    }
+});
+
+
