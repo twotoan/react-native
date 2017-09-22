234543e7e93ddbc84d62f2ffba89d4c7a19b5c58
Matthew Davies <matthew.davies@freetimers.com>
2017-09-22 15:09:15 +0100

    More prep work

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import Background from 'images/galaxy-wallpaper-30.jpg';
+import { View, StyleSheet, Button, TextInput, Text, Image } from 'react-native';
 
 export default class App extends Component {
     render() {
         return (
-            <Login></Login>
+            <View style={styles.content}>
+                <Header></Header>
+                <Login></Login>
+                <Footer></Footer>
+            </View>
         )
     }
 }
@@ -14,11 +17,13 @@ export class Login extends Component {
     render() {
         return (
             <View>
-                <Button title="Submit" accessibilityLabel="Submit Details" style={styles.buttonStyle}>Submit</Button>
+                <TextInput placeholder="Email Address" style={styles.input}/>
+                <TextInput placeholder="Name" style={styles.input} />
+                <Button title="Submit" accessibilityLabel="Submit Details" style={styles.buttonStyle} onPress={null}>Submit</Button>
             </View>
         )
     }
-},
+}
 
 export class Questions extends Component {
 
@@ -28,15 +33,66 @@ export class Scores extends Component {
 
 }
 
+export class Header extends Component {
+    render() {
+        return (
+            <View style={styles.header}>
+                <Text>ABC123</Text>
+            </View>
+        )
+    }
+}
+export class Footer extends Component {
+    render() {
+        return (
+            <View style={styles.header}>
+                <Text>ABC123</Text>
+            </View>
+        )
+    }
+}
+
+export class BackgroundImage extends Component {
+    render() {
+        const resizeMode = 'cover';
+
+        return (
+            <Image
+                style={{
+                    flex: 1,
+                    resizeMode,
+                }}
+                source={{ uri: "./images/galaxy-wallpaper-30.jpg" }}
+            />
+        );
+    }
+}
+
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: 'blue',
+        backgroundColor: '#00A2B2',
+        padding: 20,
+        textAlign: 'center',
     },
     content: {
-        backgroundImage: `url(${Background})`,
+        backgroundColor: '#FFCEAA',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
     },
     buttonStyle: {
-        backgroundColor: 'purple',
+        backgroundColor: '#B24B00',
+    },
+    footer: {
+        backgroundColor: '#19EAFF',
+        padding: 20,
+        textAlign: 'center',
+    },
+    input: {
+        margin: 20,
+        textAlign: 'center',
+        backgroundColor: '#ccc',
+        padding: 20,
     }
 });
 
